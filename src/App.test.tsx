8cc9d21cb2d9e06139f1f3eb6fb1184dab967dee
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-06-29 20:54:27 +0530

    add testing in input field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from "../src/App";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App />);
-    const headline = screen.getByText(/Hello world! My first test case./i);
-    expect(headline).toBeInTheDocument();
-  });
+test('Test Fist component', ()=> {
+  render(<App/>);
+  const test = screen.getByText(/Hello world! My first test case./i);
+  const title = screen.getByTitle("kasandbox Image");
+  expect(test).toBeInTheDocument();
+  expect(title).toBeInTheDocument()
 });
 
 
+test('Test the Input box', ()=> {
+  render(<App/>);
+  const checkInput = screen.getByRole('textbox');
+  const checkInputPlaces = screen.getByPlaceholderText("Enter User Name");  // search by placeholder name
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaces).toBeInTheDocument();   
+  expect(checkInput).toHaveAttribute('name','username');   // find by name attribute
+  expect(checkInput).toHaveAttribute("placeholder", "Enter User Name")  // find by placeholder attribute
+})
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello world! My first test case.</h1>
+      <h1>Hello world! My first test CASE.</h1>
+
+      <input type="text" 
+      placeholder="Enter User Name"
+      name = 'username'
+      />
+
+      <img 
+      title="kasandbox Image"
+      src="https://www.kasandbox.org/programming-images/avatars/cs-hopper-cool.png" />
     </div>
   )
 }

74d53089ad57ad88c25d15644dc8531d78883759
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-07-06 23:44:41 +0530

    Before all , Before Each , After Each , After all test function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 
-// skip for now
-test.skip('Test Fist component', ()=> {
+beforeAll(()=>{
+  console.log('******** before all called ************')
+});
+
+beforeEach(()=>{
+  console.log('---- before Each -----')
+})
+
+
+// afterAll
+
+test('Test Fist component', ()=> {
+  console.log('Test first component')
   render(<App/>);
   const test = screen.getByText(/Hello world! My first test case./i);
   const title = screen.getByTitle("kasandbox Image");
@@ -27,10 +38,22 @@ test.skip('Test the Input box', ()=> {
 
 // Test case for onclick event
 describe('Test case for onClick event', ()=>{
+  console.log('onchange function run')
   test('Test case 1', ()=> {
     render(<App/>);
     const btn = screen.getByRole('button');  // get button
     fireEvent.click(btn);   // add fire event
     expect(screen.getByText('updated data')).toBeInTheDocument(); // is this 'updated data' is in the document or not
   })
+})
+
+
+
+afterEach(()=>{
+  console.log("---- After Each ------")
+});
+
+
+afterAll(()=>{
+  console.log('======== After All =======')
 })

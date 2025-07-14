1b34b765800510c79fe0c8be292a3755c54cd8d7
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-07-14 23:06:44 +0530

    getByLabelText and getAllByLabelText test cases

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -11,16 +11,35 @@ const InputBox = () => {
 
             <label htmlFor="test">Test Label</label>
             <input type="text"
-            id='test'
+                id='test'
+                data-testid = 'test'
                 value={data}
+                placeholder='Enter Test Label'
                 onChange={(e) => { setData(e.target.value + 'test') }}
             />
 
 
             <label htmlFor="agree">Are You Agree</label>
             <input id='agree' type='checkbox'
-             defaultChecked={true}  // by default is wil checked unless it will give error
+                defaultChecked={true}  // by default is wil checked unless it will give error
             />
+
+
+            {/* Different user name here */}
+            <label htmlFor="user-name1">UserName</label>
+            <input type="text" id='user-name1' placeholder='Enter Username' defaultValue={'John'} />
+
+            <br />
+
+            <label htmlFor="user-name2">UserName</label>
+            <input type="text" id='user-name2' placeholder='Enter Username' defaultValue={'John'}   />
+
+            <br />
+
+            <label htmlFor="user-name3">UserName</label>
+            <input type="text" id='user-name3' placeholder='Enter Username' defaultValue={'John'}  />
+            
+            <br />
 
 
         </div>
--- src/test/Input.test.tsx
+++ src/test/Input.test.tsx
@@ -4,22 +4,36 @@ import { fireEvent, screen, render } from "@testing-library/react";
 import InputBox from "../components/InputBox";
 
 
-test('on change event testing', ()=> {
+test('on change event testing', () => {
    render(<InputBox />);
-   const input = screen.getByRole('textbox') // if type is text
-   fireEvent.change(input, {target : {value : 'a'}}) // if we fire any text
-   expect(input.value).toBe('a'+'test'); // in the return the response will be 'atest' like this added with test 
+   // const input = screen.getByRole('textbox') // if type is text
+   const input = screen.getByTestId('test')
+   fireEvent.change(input, { target: { value: 'a' } }) // if we fire any text
+   expect(input.value).toBe('a' + 'test'); // in the return the response will be 'atest' like this added with test 
 });
 
-test("Test getByLabelTest", ()=> {
+test("Test getByLabelTest", () => {
    render(<InputBox />);
    const checkLabel = screen.getByLabelText("Test Label"); // will find the lable name
    expect(checkLabel).toBeInTheDocument();
 })
 
-test('Checkbox test case', ()=>{
+test('Checkbox test case', () => {
    render(<InputBox />);
    const checkboxLabel = screen.getByLabelText("Are You Agree");
    expect(checkboxLabel).toBeInTheDocument(); // is that label is in the document or not.
    expect(checkboxLabel).toBeChecked();  // weather box is check or not 
-})
+});
+
+
+test("Test getAllByLabelText", () => {
+   render(<InputBox />);
+
+   // get from table text
+   const sameLabelTexts = screen.getAllByLabelText('UserName');
+   // expect(sameLabelText).toBeInTheDocument();
+   for (let i = 0; i < sameLabelTexts.length; i++) {
+      expect(sameLabelTexts[i]).toBeInTheDocument();
+   }
+});
+

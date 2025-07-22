b8e77a25a762f9ef992bab47baf8b769a13c0019
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-07-22 19:04:36 +0530

    act function

--- src/test/UserEvent.test.tsx
+++ src/test/UserEvent.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserEventTest from '../components/UserEventTest';
 
@@ -17,11 +17,36 @@ test("Testing User Event on onClick Event", async()=>{
 
 test("Tesing User Event on onchange event", async()=>{
 
-    userEvent.setup();
+    userEvent.setup()
     render(<UserEventTest />);
 
     const el = screen.getByRole('textbox');
     await userEvent.type(el, 'john')
     expect(screen.getByText("john")).toBeInTheDocument();
 
+});
+
+
+// sometimes we have to update state using event clicks, 
+///   const [value, setValue] = usestate('');
+// sometimes it gives error like state change will take some times and before the change assert function will call and it will give us some warning/error
+// so to sovle this issue we use act function
+// now after adding this first it will change the state then execute the assert
+
+test("Testing onchange with act function", async()=>{
+
+    userEvent.setup();
+    render(<UserEventTest />);
+
+    const input = screen.getByRole('textbox');
+
+    await act(async ()=>{
+        await userEvent.type(input, 'john');
+    })
+
+    expect(screen.getByText('john')).toBeInTheDocument();
+
 })
+
+
+

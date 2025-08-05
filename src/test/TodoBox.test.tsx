d445483e9f4d37a8ab770c5b4513c5f06ae53805
Anjali negi <113291155+Anjali4402@users.noreply.github.com>
2025-08-05 23:18:40 +0530

    write some test cases

--- src/test/TodoBox.test.tsx
+++ src/test/TodoBox.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import TodoBox from "../components/TodoBox"
 
 
 
-describe("Todo web app testing", ()=>{
+describe("Todo web app testing", () => {
 
-    test("Test initial ui", async()=>{
+    test("Test initial ui", async () => {
 
         render(<TodoBox />);
 
@@ -13,7 +13,47 @@ describe("Todo web app testing", ()=>{
 
         expect(addTaskButton).toBeInTheDocument();
 
-    })
+    });
+
+
+    test('Test normally when enter empty input field', async () => {
+
+        render(<TodoBox />);
+
+        const promptMock = vi.spyOn(window, 'prompt').mockReturnValue('');
+        const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => { });
+
+        const addTaskButton = screen.getByTestId("add-task");
+
+
+        fireEvent.click(addTaskButton);
+
+
+        expect(promptMock).toHaveBeenCalledWith('Enter Task Name');
+        expect(alertMock).toHaveBeenCalledWith('Empty Field is not Required');
+
+        promptMock.mockRestore();
+        alertMock.mockRestore();
+
+
+    });
+
+
+    it('should proceed normally if valid task name is entered', () => {
+        const promptMock = vi.spyOn(window, 'prompt').mockReturnValue('Buy Milk');
+        const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => { });
+
+        render(<TodoBox />);
+
+        const addTaskButton = screen.getByTestId("add-task");
+        fireEvent.click(addTaskButton);
+
+        expect(promptMock).toHaveBeenCalledWith('Enter Task Name');
+        expect(alertMock).not.toHaveBeenCalled();
+
+        promptMock.mockRestore();
+        alertMock.mockRestore();
+    });
 
 
 
